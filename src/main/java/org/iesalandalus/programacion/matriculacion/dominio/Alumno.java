6e5f736a1e0ad46ee7c47f37f8cf35a3d7857e0f
barzo <barzomiadam@gmail.com>
2024-12-04 21:20:16 +0100

    Añadido nombre a Readme Alumno

---
+++ src/main/java/org/iesalandalus/programacion/matriculacion/dominio/Alumno.java
@@ -0,0 +1,10 @@
+package org.iesalandalus.programacion.matriculacion.dominio;
+
+public class Alumno {
+    private static final String ER_TELEFONO ="\\d{9}";
+    private String ER_CORREO;
+    private String ER_DNI;
+    public String FORMATO_FECHA;
+    private String MR_NIA;
+
+}
