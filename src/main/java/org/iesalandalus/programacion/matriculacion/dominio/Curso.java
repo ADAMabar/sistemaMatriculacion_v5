b1052cbd5f5c75fd3332dc39ab3c9c0317e30036
barzo <barzomiadam@gmail.com>
2024-12-15 15:46:20 +0100

    Añadido nombre a Readme clase Alumno clase curso

--- src/main/java/org/iesalandalus/programacion/matriculacion/dominio/Curso.java
+++ src/main/java/org/iesalandalus/programacion/matriculacion/dominio/Curso.java
@@ -2,5 +2,23 @@ package org.iesalandalus.programacion.matriculacion.dominio;
 
 public enum Curso {
 
+PRIMERO("primero"), SEGUNDO("segundo");
+
+private String cadenaAMostrar;
+
+private Curso(String cadenaAMostrar){
+    this.cadenaAMostrar=cadenaAMostrar;
+}
+
+
+public String imprimir() {
+    return this.ordinal() + ".-" + this.cadenaAMostrar;
+}
+
+    @Override
+    public String toString() {
+        return this.cadenaAMostrar;
+    }
 
 }
+
