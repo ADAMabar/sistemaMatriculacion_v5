6888b0b167045f3a9160f0b5c1cbe3fdbe9fd2ba
barzo <barzomiadam@gmail.com>
2024-12-15 16:02:21 +0100

    Añadido nombre a Readme clase Alumno enum curso enum grado

--- src/main/java/org/iesalandalus/programacion/matriculacion/dominio/Grado.java
+++ src/main/java/org/iesalandalus/programacion/matriculacion/dominio/Grado.java
@@ -1,5 +1,20 @@
 package org.iesalandalus.programacion.matriculacion.dominio;
 
 public enum Grado {
+    GDCFGB("Grado.GDCFGB"), GDCFGM("Grado.GDCFGM"), GDCFGS("Grado.GDCFGS");
 
+private String cadenaAMostrar;
+
+private Grado(String cadenaAMostrar){
+    this.cadenaAMostrar=cadenaAMostrar;
 }
+    public String imprimir() {
+        return this.ordinal() + ".-" + this.cadenaAMostrar;
+    }
+
+    @Override
+    public String toString() {
+        return this.cadenaAMostrar;
+    }
+}
+
